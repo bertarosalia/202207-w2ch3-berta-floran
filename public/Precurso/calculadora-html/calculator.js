e59f5836dd1ebe50673b75f654d276a595e4cc9d
Berta Floran <bertafloran@hotmail.com>
2022-07-13 21:55:33 +0200

    refactored all the files

--- public/Precurso/calculadora-html/calculator.js
+++ public/Precurso/calculadora-html/calculator.js
@@ -1,75 +1,74 @@
-let currentValue= "";
-let previousValue= "";
-const display = document.getElementById ('current-value');
-const display2 = document.getElementById ('previous-value')
-const numbersButtons = document.querySelectorAll ('.number');
-const operatorButtons = document.querySelectorAll ('.operator');
-const backButton = document.querySelector ('.back');
-const eraserButton = document.querySelector ('.doubleButton');
-const equalButton = document.querySelector ('.equal');
+let currentValue = "";
+let previousValue = "";
+const display = document.getElementById("current-value");
+const display2 = document.getElementById("previous-value");
+const numbersButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const backButton = document.querySelector(".back");
+const eraserButton = document.querySelector(".doubleButton");
+const equalButton = document.querySelector(".equal");
 let result;
 let operator;
-const decimalButton= document.querySelector ('.comma');
-
+const decimalButton = document.querySelector(".comma");
 
 numbersButtons.forEach((button) => {
-button.addEventListener ('click',() => {currentValue = currentValue + button.innerText;
-   display.innerText = currentValue;
-   display2.innerText = "";
-    });
+  button.addEventListener("click", () => {
+    currentValue += currentValue + button.innerText;
+    display.innerText = currentValue;
+    display2.innerText = "";
+  });
 });
 
-operatorButtons.forEach((button) =>{
-  button.addEventListener ('click',() =>{
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
     previousValue = currentValue;
-    currentValue="";
+    currentValue = "";
     operator = button.innerText;
     display.innerText = previousValue + button.innerText;
-  })
-   });
+  });
+});
 
-const addListeners =(button) =>{
-  backButton.addEventListener ('click',() => 
-  {currentValue= currentValue.slice(0,-1).toString()
+const addListeners = (button) => {
+  backButton.addEventListener("click", () => {
+    currentValue = currentValue.slice(0, -1).toString();
     display2.innerText = "";
     display.innerText = currentValue;
-   });
- eraserButton.addEventListener ('click',() => 
- {currentValue= "";
-   display.innerText = currentValue;
-   display2.innerText = "";
-});
-equalButton.addEventListener ('click', ()=>{
-  calculate();
-    display2.innerText = result;
-    currentValue= "";
+  });
+  eraserButton.addEventListener("click", () => {
+    currentValue = "";
     display.innerText = currentValue;
-});
+    display2.innerText = "";
+  });
+  equalButton.addEventListener("click", () => {
+    calculate();
+    display2.innerText = result;
+    currentValue = "";
+    display.innerText = currentValue;
+  });
 };
 addListeners();
 
-const floatingNumbers =(button) =>{
-  decimalButton.addEventListener('click', () => { 
-   if (!currentValue.includes(".")){
-    currentValue= currentValue + ".";
-      display.innerText =  currentValue;
-    }else{
-      return
+const floatingNumbers = (button) => {
+  decimalButton.addEventListener("click", () => {
+    if (!currentValue.includes(".")) {
+      currentValue += ".";
+      display.innerText = currentValue;
     } 
-})
-};
+    });
+}
+
 floatingNumbers();
 
-const calculate=()=>{
-  switch(operator){
+const calculate = () => {
+  switch (operator) {
     case "+":
       result = +previousValue + +currentValue;
       display2.innerText = result;
-          break;
+      break;
     case "-":
-        result = +previousValue - +currentValue;
-        display2.innerText = result;
-        break;
+      result = +previousValue - +currentValue;
+      display2.innerText = result;
+      break;
     case "X":
       result = +previousValue * +currentValue;
       display2.innerText = result;
@@ -78,9 +77,6 @@ const calculate=()=>{
       result = +previousValue / +currentValue;
       display2.innerText = result;
       break;
+      default
   }
- };
-
-
-
-
+};
